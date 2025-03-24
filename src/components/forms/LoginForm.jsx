773b483ba58531c8f9ea03ad8dbc393e0de888e9
Piotr Piwowarski <piotr@piwowarski-uniet.com>
2025-03-24 09:00:29 +0100

    add login

--- src/components/forms/LoginForm.jsx
+++ src/components/forms/LoginForm.jsx
@@ -1,24 +1,56 @@
 import styles from './forms.module.css';
 import { AcceptButton, TextButton } from '../buttons/Button';
+import {useState} from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export const LoginForm = ({ setActivePage }) => {
+
+	const navigate = useNavigate();
+
+	const [error, setError] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+
+	const handleLoginBtn = async () => {
+		if(email === '' || password === '') {
+			setError('Należy wypełnić wszystkie pola formularza');
+			return;
+		}
+
+		const loginDto = {
+			email,
+			password
+		}
+
+		try {
+			const response = await axios.post('http://localhost:8080/api/accounts/login', loginDto);
+			localStorage.setItem('jwt', response.data.token);
+			localStorage.setItem('userId', response.data.accountId);
+			navigate('/dashboard');
+		} catch(e) {
+			setError('Błąd logowania');
+		}
+	}
+
 	return (
 		<div className={styles.content}>
 			<h1>Logowanie</h1>
+			{error && <p className={styles.error}>{error}</p>}
 			<label>
 				Podaj email
 				<input
 					type='email'
-					value={null}
-					onChange={null}
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
 				/>
 			</label>
 			<label>
 				Podaj hasło
 				<input
 					type='password'
-					value={null}
-					onChange={null}
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
 				/>
 			</label>
 			<p>
@@ -28,7 +60,7 @@ export const LoginForm = ({ setActivePage }) => {
 					onClick={() => setActivePage('registration')}
 				/>
 			</p>
-			<AcceptButton label='Zaloguj się' />
+			<AcceptButton label='Zaloguj się' onClick={handleLoginBtn} />
 		</div>
 	);
 };
